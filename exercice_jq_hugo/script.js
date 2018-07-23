652af793a3b848736293d8ffeb7af08a095b3635
akbar <39194143+akbar2023@users.noreply.github.com>
2018-07-23 17:59:08 +0200

    v
    v

---
+++ exercice_jq_hugo/script.js
@@ -0,0 +1,15 @@
+$(function(){
+    $('#valid').click(function(){
+        var nom = $('#nom').text();
+        var prenom = $('#prenom').text();
+        var email = $('#email').text();
+        var telephone = $('#tel').text();
+
+        $('table#MesContacts tbody').create('tr')
+
+
+
+
+    });
+
+});
